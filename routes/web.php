715d319e3da3b4a97b25d33b17448fe08e2b94bc
u85858558 <lennisth337@gmail.com>
2021-04-27 00:14:23 +0300

    add environment(redis, vue, laravel-echo-server)

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/room', function () {
+    return view('room');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
